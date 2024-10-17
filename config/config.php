cb792f5e4ce6d2ee5de02a19a3ddd1c3e2330860
Ernest Espinosa <wasp.ernest@gmail.com>
2024-10-17 23:10:52 +0800

    update - config

--- config/config.php
+++ config/config.php
@@ -1,16 +1,20 @@
 <?php
 require_once __DIR__ . '/envconfig.php';
 
-if (!defined('ENV')) define('ENV', "local");
+if (!defined('ENV')) define('ENV', "live");
 
-if (ENV == "local") :
-
+if (ENV == "local") {
     if (!defined('DB_HOST')) define('DB_HOST', Env::get('LOCAL_DB_HOST'));
     if (!defined('DB_NAME')) define('DB_NAME', Env::get('LOCAL_DB_NAME'));
     if (!defined('DB_USER')) define('DB_USER', Env::get('LOCAL_DB_USER'));
     if (!defined('DB_PASS')) define('DB_PASS', Env::get('LOCAL_DB_PASS'));
+} else {
+    if (!defined('DB_HOST')) define('DB_HOST', Env::get('LIVE_DB_HOST'));
+    if (!defined('DB_NAME')) define('DB_NAME', Env::get('LIVE_DB_NAME'));
+    if (!defined('DB_USER')) define('DB_USER', Env::get('LIVE_DB_USER'));
+    if (!defined('DB_PASS')) define('DB_PASS', Env::get('LIVE_DB_PASS'));
+}
 
-endif;
 
 try {
     global $dbh;  // Make the $dbh global
